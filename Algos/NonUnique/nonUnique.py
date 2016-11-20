08a9b452ab34bd5333a0a960ad3b43a88da728c3
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-11-20 13:39:30 +0530

    Update nonUnique.py

--- Algos/NonUnique/nonUnique.py
+++ Algos/NonUnique/nonUnique.py
@@ -1,5 +1,5 @@
 def nonUnique (data):
-    data = [i for i in data if data.count (i) > 1]
+    data = [i for i in data if data.count(i) > 1]
     return data
     
 print (nonUnique ([1, 2, 3, 3, 1]))
